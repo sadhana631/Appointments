9e69957832ec3ba0a53446f6bfbcc7150689001f
sadhana631 <indhusweety631@gmail.com>
2024-07-11 07:14:03 +0000

    first commit

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,141 @@
+import {Component} from 'react'
+import {v4} from 'v4id'
+import {format} from 'date-fns'
+
+import AppointmentItem from './AppointmentsItem'
+
+import './index.css'
+
+class Appoinments extends Component {
+  state = {
+    appoinmentsList: [],
+    titleInput: '',
+    dateInput: '',
+    isFilterActive: false,
+  }  
+
+  toggleIsStarred = id => {
+    this.setState(prevState => ({
+      appoinmentsList: prevState.appoinmentsList.map(eachAppointment => {
+        if (id === eachAppointment) {
+         return {...eachAppointment, isStarred: eachAppointment.isStarred}   
+        }  
+        return eachAppointment
+      }),
+    }))
+  }
+
+  onFilter = () => {
+    const {isFilterActive} = this.state
+
+    this.setState({
+      isFilterActive: !isFilterActive,
+    }) 
+  }
+
+  onChangeDateInput = event => {
+    this.setState({dateInput: event.target.value})
+  }
+
+  onChangeTitleInput = event => {
+    this.setState({titleInput: event.target.value})           
+  }
+
+  onAddAppointment = event => {
+    event.preventDefault()
+    const {titleInput, dateInput} = this.state
+    const formattedDate = dateInput
+      ? format(new Data(dateInput), 'dd MMMM yyyy, EEEE')
+      ? ''
+    const newAppointment = {
+      id: v4(),
+      title: titleInput,
+      date: formattedDate,
+      isStarred: false,
+    }
+
+    this.setState(prevState => ({
+      appoinmentsList: [...prevState.appoinmentsList, newAppointment],
+      titleInput: '',
+    }))
+  }
+
+  getfilteredAppointmentsList = () => {
+    const {appoinmentsList, isFilterActive} =  this.state
+
+    if (isFilterActive) {
+      return appoinmentsList.filter(
+        eachTransaction => eachTransaction.isStarred === true,
+      )
+    }
+      return appoinmentsList
+  }
+
+  render() {
+    const {titleInput, dateInput, isFilterActive} = this.state
+    const filterClassName = isFilterActive  ? 'filter-filled' 'filter-empty'
+    const filteredAppointmentsList = this.getfilteredAppointmentsList()
+
+    return (
+      <div className="app-container">
+       <div className="responsive-container">
+        <div className="appointment-container">
+         <form className="form" onSubmit={this.onAddAppointment}>
+          <h1 className="add-appointment-heading">Add Appoinment</h1>
+          <label htmlFor="title" className="label">
+            TITLE
+          </label>
+          <input
+            type="text"
+            id="title"
+            value={titleInput}
+            onChange={this.onChangeTitleInput}
+            className="input"
+            placeholder="Title"
+          />
+          <label htmlFor="date" className="label">
+            DATE
+          </label>  
+          <input
+            type="date"
+            id="date"
+            value={dateInput}
+            className="input"
+          />
+          <button type="submit" className="add-button">
+            Add
+          </button>       
+        </form>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
+          alt="appointments"
+          className="appointments-img"
+        />  
+       </div>
+       <hr className="hr" />
+        <div className="header-with-filter-container">
+         <h1 className="appointments-heading">Appoinments</h1>
+         <button
+           type="button"
+           className={`filter-style ${filterClassName}`}
+           onClick={this.onFilter}
+         >
+           Starred
+         </button>  
+        </div>
+        <ul className="appointments-list">
+          {filteredAppointmentsList.map(eachAppointment => (
+            <AppointmentItem
+              key={eachAppointment.id}
+              appointmentDetails={eachAppointment}
+              toggleIsStarred={this.toggleIsStarred}
+            />
+          ))}    
+        </ul>
+       </div>
+      </div>  
+    )
+  }
+}
+
+export default Appoinments
